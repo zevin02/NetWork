7bac005dc84a7a945fcc8e34f792e43b8f18ed70
zevin02 <1833195090@qq.com>
2022-07-10 21:00:34 +0800

    多进程使用孙子进程解决子进程退出的问题阻塞和信号的问题

--- TCP/server.cc
+++ TCP/server.cc
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include<cstdlib>
 #include<signal.h>
+#include<string>
+#include<sys/wait.h>
 using namespace std;
 
 void serverio(int newsockfd)
@@ -65,7 +67,7 @@ int main()
     {
         cerr << "listen error" << endl;
     }
-    signal(SIGCHLD,SIG_IGN);//子进程退出的时候会给父进程发送信号，这里我们去忽略一下子进程发送的信号,子进程会自动退出释放资源
+    // signal(SIGCHLD,SIG_IGN);//子进程退出的时候会给父进程发送信号，这里我们去忽略一下子进程发送的信号,子进程会自动退出释放资源
     while (1)
     {
         struct sockaddr_in peer;
@@ -78,22 +80,36 @@ int main()
         }
         //我们这里现在是一个单进程，一次只能允许一个人使用
         //我们使用多进程来操作一下
-
+        uint16_t cli_port=ntohs(peer.sin_port);//保证代码的可移植性
+        string ip=inet_ntoa(peer.sin_addr);//
+        cout<<"get a link -> : "<<cli_port<<" : "<<newsockfd<<" "<<ip<<endl;//我们发现每一个都是4
         pid_t id = fork();
         if (id < 0)
         {
             continue;
         }
         else if (id == 0)
+        //曾经被父进程打开的fd，是否会被子进程继承？无论父子进程的哪一个，强烈建议关闭不需要的fd，监听描述符也会被子进程看到，
+        //我们在子进程这边把监听描述符关闭
         {
             // child
+            close(sockfd);
+            if(fork()>0)
+            exit(0);//这个叫做退出的是子进程，向后走的进程是孙子进程
+
+            //因为父进程一进来就挂掉了，所以孙子进程就变成了孤儿进程，后会被操作系统给回收掉，
             serverio(newsockfd);
+            //如果不关闭文件描述符，就会导致文件描述符泄露的问题
+            close(newsockfd);
             exit(1);//子进程执行完了，父进程就要等
             //子进程和父进程的文件描述符是共享的，退出后要关闭文件描述符
         }
         else
         {
             //do nothing ,因为父进程忽略了，所以就不需要去wait，父进程不断的建立连接，子进程不断的提供服务
+            //阻塞等待不需要等待，耗时间很大，所以使用忽略sigchild信号，
+            waitpid(id,nullptr,0);//这里等待的时候会不被阻塞等待太长时间，因为它子进程刚一创建出来就退出了孙子进程执行完之后就被OS给回收了
+            close(newsockfd);//fork已经返回了
         }
         //提供服务
     }
